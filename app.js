f754bde2fbdefa011dbf361a8f500cd817d0f36b
Srar <a1020938070@gmail.com>
2016-03-19 01:44:18 +0800

    add get link cache

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ var superagent    = require('superagent');
 var fs            = require('fs');
 var app           = express();
 
+var Cache = {};
+setInterval(() => {
+    Object.keys(Cache).forEach((key) => {
+        var obj = Cache[key];
+        obj.time--;
+        if(obj.time <= 0) delete Cache[key];
+    });
+}, 1000);
+
 app.use(logger('dev'));
 app.use(require('body-parser')());
 app.use((req, res, next) => {
     req.config     = require('./config.json');
     req.getApiData = (url, data, callback) => {
-        
         if(req.config.accessToken.trim() == '') return callback('请先绑定189电信账号');
         
         data['app_id'] = req.config.appKey
         data['access_token'] = req.config.accessToken;
         
-        superagent.get(url).query(data).end((err, res) => {
+        superagent.get(url).query(data).timeout(1000 * 10).end((err, res) => {
             if(err) return callback(err);
             try {
                 callback(null, JSON.parse(res.text));
@@ -24,6 +32,14 @@ app.use((req, res, next) => {
             }
         });
     }
+    
+    req.setCache = (key, obj, time) => {
+        Cache[key] = {time: time, data: obj}
+    }
+    req.getCache = (key) => {
+        return Cache[key] ? Cache[key] : null;
+    }
+    
     next();
 });
 
@@ -62,8 +78,18 @@ app.get('/', (req, res) => {
 
 app.get('/link/:fileId', (req, res) => {
     var fileId = req.params.fileId;
+    var cache = req.getCache(fileId);
+    if(cache){
+        res.set('Link-Cache', 'Hit ' + cache.time);
+        return res.redirect(cache.data);
+    }
     req.getApiData('http://api.189.cn/ChinaTelecom/getFileDownloadUrl.action', {fileId: fileId}, (err, result) => {
         if(err) return res.status(503).send(err);
+        // 天翼云盘真实链接并不会立即过期，也许要过1天? 或者100年? 
+        // 反正这里配置文件里默认缓存60分钟 因为我就试了60分钟 懒得试了 但是证实60分钟+还是有效的 
+        // 如果电信修改了 请自行修改 config -> linkCacheTime
+        if(req.config.linkCache) req.setCache(fileId, result.fileDownloadUrl, req.config.linkCacheTime);
+        res.set('Link-Cache', 'Miss');
         res.redirect(result.fileDownloadUrl);
     });
 });
